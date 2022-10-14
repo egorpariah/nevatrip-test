3e692b4b255ad650c7e8d48f893eb8a9c6930fcc
Egor Golikov <elai.asti@gmail.com>
2022-10-15 06:08:36 +0700

    Исправил путь к странице «Маршрут»

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ git init
 git add -A
 git commit -m 'deploy'
 git branch -M main
-git push -f https://github.com/egorpariah/vue3-blog-app.git main:gh-pages
+git push -f https://github.com/egorpariah/nevatrip-test.git main:gh-pages
 
 cd -
